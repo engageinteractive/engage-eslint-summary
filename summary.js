11505208d584ace0e1467cc7cf59b0f1f650e425
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2016-03-23 17:09:21 +0000

    Tidies up the js.

--- summary.js
+++ summary.js
@@ -3,52 +3,65 @@
 var chalk = require('chalk');
 
 var pluralize = function(word, count) {
-    var plural = count === 1 ? word : word + 's';
-    return plural;
+
+	var plural = count === 1 ? word : word + 's';
+	return plural;
+
 };
 
 module.exports = function(results) {
 
-    var errorColor = 'red';
-    var warningColor = 'yellow';
+	var errorColor = 'red';
+	var warningColor = 'yellow';
 
-    var errorCount = 0;
-    var fileCount;
-    var failureCount = 0;
-    var passCount = 0;
-    var warningCount = 0;
+	var errorCount = 0;
+	var fileCount;
+	var failureCount = 0;
+	var passCount = 0;
+	var warningCount = 0;
 
-    var summaryLineArray;
+	var summaryLineArray;
 
+	results.forEach(function(result) {
 
-    results.forEach(function(result) {
+		var messages = result.messages;
 
-        var messages = result.messages;
+		if (messages.length === 0) {
 
-        if (messages.length === 0) {
-            passCount++;
-        } else {
-            failureCount++;
-            warningCount += result.warningCount;
-            errorCount += result.errorCount;
-        }
+			passCount++;
 
-    });
+		}
+		else {
 
-    fileCount = passCount + failureCount;
+			failureCount++;
+			warningCount += result.warningCount;
+			errorCount += result.errorCount;
 
-    summaryLineArray = [
-        'JavaScript Lint Summary:',
-        chalk.bold(fileCount + ' ' + pluralize('file', fileCount) + ' checked.'),
-        chalk.bold(passCount + ' passed.'),
-        chalk.bold(failureCount + ' failed.')
-    ];
+		}
 
-    if (warningCount || errorCount) {
-        summaryLineArray.push(chalk[warningColor].bold(warningCount + ' ' + pluralize('warning', warningCount) + '.'));
-        summaryLineArray.push(chalk[errorColor].bold(errorCount + ' ' + pluralize('error', errorCount) + '.'));
-    }
+	});
 
-    return summaryLineArray.join(' ');
+	fileCount = passCount + failureCount;
+
+	summaryLineArray = [
+		'JavaScript Lint Summary:',
+		chalk.bold(fileCount + ' ' + pluralize('file', fileCount) + ' checked.'),
+		chalk.bold(passCount + ' passed.'),
+		chalk.bold(failureCount + ' failed.')
+	];
+
+	if (warningCount || errorCount) {
+
+		summaryLineArray.push(chalk[warningColor].bold(warningCount
+			+ ' ' + pluralize('warning', warningCount) + '.')
+		);
+
+		summaryLineArray.push(chalk[errorColor].bold(errorCount
+			+ ' ' + pluralize('error', errorCount) + '.')
+		);
+
+	}
+
+	return summaryLineArray.join(' ');
 
 };
