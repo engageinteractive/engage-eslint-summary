7529cd8374d9398c3d6800560891ea23af530652
Adam Gilleard <adam.gilleard@engageinteractive.co.uk>
2017-01-31 10:12:03 +0000

    Adds options, allow for hiding error details.

--- index.js
+++ index.js
@@ -43,95 +43,97 @@ var docsUrl = function(message) {
 
 };
 
-module.exports = function(results) {
+module.exports = function(opts) {
+	return function(results) {
 
-	var
-		summary = [],
-		counts = {
-			file: 0,
-			error: 0,
-			failure: 0,
-			pass: 0,
-			warning: 0
-		},
-		error = false;
+		var
+			summary = [],
+			counts = {
+				file: 0,
+				error: 0,
+				failure: 0,
+				pass: 0,
+				warning: 0
+			},
+			error = false;
 
-	results.forEach(function(result) {
+		results.forEach(function(result) {
 
-		if (result.messages.length) {
+			if (result.messages.length) {
 
-			counts.failure++;
-			counts.warning += result.warningCount;
-			counts.error += result.errorCount;
+				counts.failure++;
+				counts.warning += result.warningCount;
+				counts.error += result.errorCount;
 
-			if (result.errorCount && !error) {
-				error = result;
+				if (result.errorCount && !error) {
+					error = result;
+				}
+
+			} else {
+
+				counts.pass++;
+
 			}
 
-		} else {
+		});
 
-			counts.pass++;
+		counts.file = counts.pass + counts.failure;
 
+		summary.push([
+			'JaveScript Summary:',
+			counts.file + ' ' + pluralize('file', counts.file) + ' checked.',
+			counts.pass + ' passed.'
+		].join(' '));
+
+		if (counts.error || counts.warning) {
+			summary.push(chalk.yellow.bold(counts.failure + ' failed.'));
 		}
 
-	});
+		['error', 'warning'].forEach(function(type) {
 
-	counts.file = counts.pass + counts.failure;
+			if (counts[type]) {
 
-	summary.push([
-		'JaveScript Summary:',
-		counts.file + ' ' + pluralize('file', counts.file) + ' checked.',
-		counts.pass + ' passed.'
-	].join(' '));
+				summary.push(
+					chalk[type === 'error' ? 'red' : 'yellow'].bold([
+						counts[type],
+						pluralize(type, counts[type]) + '.'
+					].join(' '))
+				);
 
-	if (counts.error || counts.warning) {
-		summary.push(chalk.yellow.bold(counts.failure + ' failed.'));
-	}
-
-	['error', 'warning'].forEach(function(type) {
-
-		if (counts[type]) {
-
-			summary.push(
-				chalk[type === 'error' ? 'red' : 'yellow'].bold([
-					counts[type],
-					pluralize(type, counts[type]) + '.'
-				].join(' '))
-			);
-
-		}
-
-	});
-
-	if (error) {
-
-		for (var i = 0; i < error.messages.length; i++) {
-
-			if (error.messages[i].severity <= 1) {
-				continue;
 			}
 
-			summary.push([
-				'\r\n' + chalk.red(path.parse(error.filePath).base),
-				chalk.red(error.messages[i].line + ':' + error.messages[i].column),
-				chalk.red.bold(error.messages[i].message),
-			].join(' '));
+		});
 
-			var url = docsUrl(error.messages[i]);
+		if (error && !opts.hideErrors) {
 
-			if (url) {
+			for (var i = 0; i < error.messages.length; i++) {
+
+				if (error.messages[i].severity <= 1) {
+					continue;
+				}
+
 				summary.push([
-					'\r\nSee: ',
-					url
-				].join(''));
-			}
+					'\r\n' + chalk.red(path.parse(error.filePath).base),
+					chalk.red(error.messages[i].line + ':' + error.messages[i].column),
+					chalk.red.bold(error.messages[i].message),
+				].join(' '));
 
-			break;
+				var url = docsUrl(error.messages[i]);
+
+				if (url) {
+					summary.push([
+						'\r\nSee: ',
+						url
+					].join(''));
+				}
+
+				break;
+
+			}
 
 		}
 
-	}
+		return summary.join(' ');
 
-	return summary.join(' ');
-
+	};
 };
